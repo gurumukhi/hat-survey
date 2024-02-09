b6bcd4bc1911be9e129be39cb36026fca3de71d4
Ram <vaishnav.rd@gmail.com>
2024-02-09 23:37:27 +0530

    final changes

--- v2/scripts.js
+++ v2/scripts.js
@@ -15,16 +15,26 @@ function addInputGroup () {
     document.querySelector('#removeBtn').style.opacity = 1
   } else {
     var clone = document.getElementById('inputGroup2').cloneNode(true)
+    clone.querySelector('#animalInatMetaId').value = ''
+    clone.querySelector('#animalCount').value = ''
+    clone.querySelector('#animalRemark').value = ''
     document.getElementById('inputsGroups').appendChild(clone)
   }
 }
 
-function removeInputGroup () {
-  console.log('removing')
+function removeInputGroup (e) {
+  // return
   var inputGroup2List = document.querySelectorAll('#inputGroup2')
   if (inputGroup2List.length > 1) {
-    var element = inputGroup2List[inputGroup2List.length - 1]
-    element.parentNode.removeChild(element)
+    if (e) {
+      console.log('removing')
+      console.log(e.closest('#inputGroup2'))
+      el = e.closest('#inputGroup2')
+      el.parentNode.removeChild(el)
+    } else {
+      var element = inputGroup2List[inputGroup2List.length - 1]
+      element.parentNode.removeChild(element)
+    }
   } else if (inputGroup2List.length == 1) {
     var element = inputGroup2List[inputGroup2List.length - 1]
     element.style.display = 'none'
@@ -41,18 +51,16 @@ function removeInputGroup () {
 }
 
 function submitForm () {
-  alert('Submissions are disabled for now')
   try {
-    console.log('hi')
-    var url1 =
-      'https://docs.google.com/forms/d/e/1FAIpQLSep1hYt4lOvIJSpwcsXEWT3VIqN3LRFioN0mml1awGwlIADPA/formResponse?&submit=Submit?usp=pp_url'
-    var url2 =
-      'https://docs.google.com/forms/d/e/1FAIpQLSc_dlM2JJw19OU20IzTaQmUoq5TNB9Tz0EdAptPDk7Th2hybQ/formResponse?&submit=Submit?usp=pp_url'
+    // var url1 =
+    //   'https://docs.google.com/forms/d/e/1FAIpQLSep1hYt4lOvIJSpwcsXEWT3VIqN3LRFioN0mml1awGwlIADPA/formResponse?&submit=Submit?usp=pp_url'
+    // var url2 =
+    //   'https://docs.google.com/forms/d/e/1FAIpQLSc_dlM2JJw19OU20IzTaQmUoq5TNB9Tz0EdAptPDk7Th2hybQ/formResponse?&submit=Submit?usp=pp_url'
     var url3 =
       'https://docs.google.com/forms/d/e/1FAIpQLSduzoPUnh1-VlaGDAXs4Q4oMSxQT06Es3vq-ik6XqhYXzzRZQ/formResponse?&submit=Submit?usp=pp_url'
 
-    console.log('data1')
     var formData1 = {
+      volunteerName: document.getElementById('volunteerName').value,
       teamCode: document.getElementById('teamCode').value,
       plantCode: document.getElementById('plantCode').value,
       speciesName: document.getElementById('speciesName').value,
@@ -60,57 +68,127 @@ function submitForm () {
       treeRemark: document.getElementById('treeRemark').value,
       gbh: document.getElementById('gbh').value,
       treeLeaves: document.getElementById('treeLeaves').value,
-      treeCanopy: document.getElementById('treeCanopy').value,
+      // treeCanopy: document.getElementById('treeCanopy').value,
       treeFlowering: document.getElementById('treeFlowering').checked,
       treeFruiting: document.getElementById('treeFruiting').checked
     }
 
     var inputGroup2Elements = document.querySelectorAll('#inputGroup2')
-    var formData2Array = []
 
-    inputGroup2Elements.forEach(function (group) {
-      var data = {
-        teamCode: document.getElementById('teamCode').value,
-        plantCode: document.getElementById('plantCode').value,
-        speciesName: group.querySelector('[name="animalSpecies"]').value,
-        plantInatMetaId: group.querySelector('[name="animalInatMetaId"]').value,
-        treeRemark: group.querySelector('[name="animalRemark"]').value,
-        gbh: group.querySelector('[name="animalGbh"]').value,
-        treeLeaves: group.querySelector('[name="animalLeaves"]').value,
-        treeCanopy: group.querySelector('[name="animalCanopy"]').value,
-        treeFlowering: group.querySelector('[name="animalFlowering"]').checked,
-        treeFruiting: group.querySelector('[name="animalFruiting"]').checked
+    // if (!inputGroup2Elements.length) {
+    /////// this case will never occur
+    // alert('no animal')
+    //   submitData(url3, formData1)
+    // } else
+    //  if (
+    //   inputGroup2Elements.length === 1 &&
+    //   inputGroup2Elements[0].style.display == 'none'
+    // ) {
+    if (hasNoAnimal()) {
+      // alert('still no animal')
+      submitData(url3, formData1)
+    } else {
+      // alert('ANIMAL FOUND')
+      {
+        inputGroup2Elements.forEach(function (group) {
+          console.log(group.querySelector('[name="animalSpecies"]').value)
+          console.log(group.querySelector('[name="animalCount"]').value)
+          console.log(group.querySelector('[name="interactionType"]').value)
+          console.log(group.querySelector('[name=animalInatMetaId]').value)
+          console.log(group.querySelector('[name=animalRemark]').value)
+
+          var formData2 = {
+            animalSpecies: group.querySelector('[name="animalSpecies"]').value,
+            animalCount: group.querySelector('[name="animalCount"]').value,
+            interactionType: group.querySelector('[name="interactionType"]')
+              .value,
+            animalInatMetaId: group.querySelector('[name=animalInatMetaId]')
+              .value,
+            animalRemark: group.querySelector('[name=animalRemark]').value
+          }
+          submitData(url3, { ...formData1, ...formData2 })
+        })
       }
-      formData2Array.push(data)
-    })
-
-    console.log('data2')
-    formData1['data2'] = formData2Array
-
-    console.log('submitting1')
-    submitData(url1, formData1)
-
-    console.log('submitting2')
-    submitData(url2, formData1)
-
-    console.log('submitting3')
-    submitData(url3, formData1)
+    }
+    alert('Data submitted successfully.')
+    next()
   } catch (e) {
     console.error(e)
     alert(e)
   }
 }
 
+next = function () {
+  while (!hasNoAnimal()) {
+    removeInputGroup()
+  }
+
+  document.getElementById('plantCode').value =
+    Number(document.getElementById('plantCode').value) + 1
+  document.getElementById('speciesName').value = 'Unidentified / Other'
+  document.getElementById('plantInatMetaId').value = ''
+  document.getElementById('gbh').value = ''
+  document.getElementById('treeLeaves').value = 'Unsure'
+  // document.getElementById('treeCanopy').value = ''
+  document.getElementById('treeFlowering').checked = false
+  document.getElementById('treeFruiting').checked = false
+  document.getElementById('treeRemark').value = ''
+  ////
+}
+
 function submitData (url, data) {
-  fetch(url, {
+  // return `${url}&entry.1731982848=${data.teamCode}&entry.1336780329=${data.plantCode}&entry.1634616229=${data.speciesName}&entry.965392427=${data.gbh}`;
+  // return `${url}&entry.649217680=${data.associatedPlantCode}&entry.267793795=${data.animalSpecies}&entry.342920962=${data.animalCount}&entry.1901368330=${data.interactionType || "Feeding"}`
+
+  // teamCode
+  // plantCode
+  // speciesName
+  // plantInatMetaId
+  // treeRemark
+  // gbh
+  // treeLeaves
+  // treeCanopy
+  // treeFlowering
+  // treeFruiting
+  // animalSpecies
+  // animalCount
+  // interactionType
+  // animalInatMetaId
+  // animalRemark
+
+  // var url3Suffix = '&entry.1664808834=teamcode&entry.1981805346=treecode&entry.798862294=treespeciesname&entry.724347484=treeinatid&entry.1957343692=treeremarks&entry.1423009416=treegbh&entry.218206116=treeleaves&entry.1767881883=treecanopy&entry.1792652260=treeflowering&entry.1156221592=treefruiting&entry.1342359126=animalname&entry.1739765474=animalinatid&entry.1976377500=animalremarks&entry.1717792239=animalcount&entry.1852332458=animalactivity '
+
+  // https://docs.google.com/forms/d/e/1FAIpQLSduzoPUnh1-VlaGDAXs4Q4oMSxQT06Es3vq-ik6XqhYXzzRZQ/viewform?usp=pp_url&entry.1664808834=teamCode&entry.1981805346=treeCode&entry.798862294=treeSpeciesName&entry.724347484=treeIatMetaId&entry.1957343692=treeRem&entry.1423009416=treeGbh&entry.218206116=treeLeaves&entry.1767881883=treeCanopy&entry.1792652260=treeFlowering&entry.1156221592=treeFruiting&entry.1342359126=animalName&entry.1739765474=aniamlInat&entry.1976377500=animalRem&entry.1717792239=animalCount&entry.1852332458=animalActivity
+
+  var url3Suffix = `&entry.1664808834=${data.teamCode}&entry.1981805346=${
+    data.plantCode
+  }&entry.798862294=${data.speciesName}&entry.724347484=${
+    data.plantInatMetaId
+  }&entry.1957343692=${data.treeRemark}&entry.1423009416=${
+    data.gbh
+  }&entry.218206116=${data.treeLeaves}&entry.1767881883=${
+    data.treeCanopy || ''
+  }&entry.1792652260=${data.treeFlowering}&entry.1156221592=${
+    data.treeFruiting
+  }&entry.1342359126=${data.animalSpecies || ''}&entry.1739765474=${
+    data.animalInatMetaId || ''
+  }&entry.1976377500=${data.animalRemark || ''}&entry.1717792239=${
+    data.animalCount || ''
+  }&entry.1852332458=${data.interactionType || ''}&entry.455418864=${
+    data.volunteerName
+  } `
+
+  var finalURL = `${url}${url3Suffix}`
+
+  fetch(finalURL, {
     method: 'POST',
     mode: 'no-cors',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    body: encodeFormData(data)
+    }
+    // body: encodeFormData(data)
   })
-    .then(response => console.log('Form submitted successfully'))
+    .then(response => console.log(response))
     .catch(error => console.error('Error submitting the form', error))
 }
 
@@ -146,126 +224,209 @@ function loadTreeData () {
   //   .then(csvData => {
   // Parse CSV to JSON using Papaparse
   csvData = `CommonName
-African tulip tree
-Amla
-Arjuna
-Axlewood
-Babool
-Badminton-ball tree
-Banyan
-Beheda
-Ber
-Black myrobalan
-Bottle brush
-Broken Bones Tree
-Cannonball Tree
-Caribbean trumpet tree
-Champa
-Chandada
-Chiku sapodilla
-Chinaberry
-Chinese Lantern Tree
-Chirauli Nut
-Coconut Palm
-Copperpod
-Country fig
-Crepe jasmine
-Croaya
-Curry-leaf
-Custard Apple
-Devil Tree
-Drumstick tree
-Dyers oleander
-East-Indian satinwood
-Elephant Apple
-False white teak
-Fish-tail Palm
-Five-leaved Vitex
-Flame of the Forest
-Floss Silk Tree
-Frangipani
-Gamar
-Garlic Pear Tree
-Garuga
-Ghost tree
-Guava
-Guh-de
-Gulmohur
-Henna
-Indian almond
-Indian ash tree
-Indian coral tree
-Indian cork tree
-Indian elm
-Indian Frankinscence
-Indian Kino
-Indian Laburnum
-Indian laurel
-Indian Mulberry
-Indian Prickly Ash
-Indian rosewood
-Indian tulip tree
-Jacaranda
-Jackfruit
-Jamun
-Jhinjheri
-Jungle jalebi
-Kadamb
-Kaim
-Kanak champa
-Khadi aam
-Khajur
-Khejri
-Krishna Siris
-Lantana
-Lychee
-Mahua
-Mango (all varieties)
-Mast tree
-Moulsari
-Mountain Coffee
-Nag champa
-Neem
-Night-flowering jasmine
-Noni
-Oleander
-Oriental cashew
-Peepal
-Pink poui
-Pink-pearl tree
-Pomegranate
-Pomelo
-Pongam
-Powderpuff Mangrove
-Pride of India
-Punjab Fig
-Purple Bauhinia
-Quickstick
-Rain tree
-Red Silk Cotton
-Robusta Coffee
-Roheda
-Roxburgh's Kydia
-Sandalwood
-Sausage tree
-Silkworm mulberry
-Singapore cherry
-Siris
-Soft fig
-Takoli
-Tamarind
-Teak
-Toddy Palm
-Tree of Heaven
-True Ashoka
-Vilayati keekar
-White Babool
-White Silk Cotton
-White Siris
-Wild almond
-Wood Apple
-Yellow Oleander
-Yellow Silk Cotton
+Acacia auriculiformis (Earpod Wattle)
+Acacia holosericea (Strap-leaved Wattle)
+Acacia leucophloea (White Babool)
+Acacia nilotica (Babool)
+Adansonia digitata (Baobab)
+Adenanthera pavonia (Red Bead Tree)
+Aegle marmelos (Indian Bael)
+Ailanthus excelsa (Tree of Heaven)
+Alangium salvifolium (Sage-leaved Alangium)
+Albizia amara (Krishna Siris)
+Albizia lebbeck (Siris)
+Albizia procera (White Siris)
+Alstonia scholaris (Saptaparni/Scholar Tree)
+Annona squamosa (Custard Apple)
+Anogeissus latifolia (Axlewood)
+Artocarpus altilis (Breadfruit)
+Artocarpus heterophyllus (Jackfruit)
+Azadirachta indica (Neem)
+Barringtonia acutangula (Freshwater Mangrove)
+Barringtonia racemosa (Powderpuff Mangrove)
+Bauhinia purpurea (Purple Bauhinia)
+Bauhinia racemosa (Jhinjheri)
+Bauhinia x blakeana (Hong Kong Orchid Tree)
+Bombax ceiba (Red Silk Cotton)
+Borassus flabellifer (Toddy Palm)
+Boswellia serrata (Indian Frankinscence)
+Bridellia montana (Mountain Bridelia)
+Brownea grandiceps (Rose of Venezuela)
+Buchanania cochinchinensis (Chirauli Nut)
+Bursera penicillata (Yucatan Rosewood)
+Butea monosperma (Flame of the Forest)
+Canophyllum inophyllum (Alexandrian Laurel)
+Careya arborea (Wild Guava)
+Caryota urens (Fish-tail Palm)
+Cascabela thevetia (Yellow Oleander)
+Cassia fistula (Indian Laburnum)
+Cassia grandis (Pink Shower Tree)
+Cassia javanica (Apple Blossom Cassia)
+Cassia roxburghii (Red Cassia)
+Cassine glauca (Native Olive)
+Casuarina equisetifolia (Casuarina)
+Ceiba pentandra (White Silk Cotton)
+Ceiba speciosa (Floss Silk Tree)
+Cerbera odollum (Khadi aam)
+Chloroxylon swietenia (East-Indian satinwood)
+Citharexylum spinosa (Fiddlewood)
+Citrus maxima (Pomelo)
+Clusia rosea (Autograph Tree)
+Cochlospermum religiosum (Yellow Silk Cotton)
+Cocos nucifera (Coconut Palm)
+Coffea arabica (Mountain Coffee)
+Coffea robusta (Robusta Coffee)
+Conocarpus lancifolius (Button Mangrove)
+Cordia dichotoma (Pink-pearl Tree/Snot Berry)
+Cordia macleodi (Macleod Cordia)
+Cordia sebastena (Scarlet Cordia)
+Couroupita guianensis (Cannonball Tree)
+Crateva adansonii subsp. odora (Garlic Pear Tree)
+Dalbergia lanceolaria subsp. lanceolaria (Takoli)
+Dalbergia lanceolaria subsp. paniculata (Dhobin/Phanashi)
+Dalbergia latifolia (Black Rosewood)
+Dalbergia sissoo (Indian rosewood)
+Delonix alata (White Gulmohur)
+Delonix regia (Gulmohur)
+Dichrostachys cinerea (Chinese Lantern Tree)
+Dillenia indica (Elephant Apple)
+Diospyros chloroxylon (Green Ebony Persimmon)
+Diospyros melanoxylon (Coromandel Ebony)
+Diospyros montana (Bombay Ebony)
+Dolichandrone falcata (Medhshingi)
+Ealeocarpus serratus (Ceylon olive)
+Ehretia laevis (Koda)
+Erythrina variegata (Indian Coral Tree)
+Ficus amplissima (Indian Bat Tree)
+Ficus arnottiana (Indian Rock Fig)
+Ficus benghalensis (Banyan)
+Ficus benjamina (Weeping Fig)
+Ficus hispida (Hairy Fig)
+Ficus microcarpa (Laurel Fig)
+Ficus mollis (Soft fig)
+Ficus palmata (Punjab Fig)
+Ficus racemosa (Country fig)
+Ficus religiosa (Peepal)
+Firmiana colorata (Scarlet Sterculia)
+Flacourtia indica (Governor's Plum)
+Garuga pinnata (Garuga)
+Givotia moluccana (White Catamaran)
+Gliricidia sepium (Quickstick)
+Gmelina arborea (Gamhar)
+Grevillea robusta (Silky Oak)
+Grewia tiliifolia (Dhaman)
+Guazuma ulmifolia (West Indian Elm)
+Gyrocarpus americanus (Helicopter Tree)
+Haldinia cordifolia (Haldu)
+Heterophragma quadriloculare (Waras)
+Holarrhena pubescens (Bitter Oleander)
+Holoptelea integrifolia (Indian Elm)
+Hydroanthus impetiginosa (PinkTrumpet Tree)
+Jacaranda mimosifolia (Jacaranda)
+Khaya senegalensis (African Mahogany)
+Kigelia africana (Sausage Tree)
+Kleinhovia hospita (Guest Tree)
+Kydia calycina (Roxburgh's Kydia)
+Lagerstroemia floribunda (Thai Crape Myrtle)
+Lagerstroemia parviflora (Small-flowered Crape Myrtle)
+Lagerstroemia speciosa (Pride of India)
+Lannea coromandelica (Indian Ash)
+Lantana camara (Lantana)
+Lawsonia inermis (Henna)
+Limonia acidissima (Wood Apple)
+Litchi chinensis (Lychee)
+Macaranga peltata (Shield-leaf Tree)
+Madhuca longifolia (Mahua)
+Magnolia champaka (Champa)
+Mallotus philippensis (Kamala Tree)
+Mangifera indica (Mango (all varieties))
+Manilkara hexandra (Ceylon Ironwood)
+Manilkara zapota (Chiku sapodilla)
+Markhamia lutea (Nile Tulip)
+Melaleuca viminalis (Bottle brush)
+Melia dubia (Malabar Neem)
+Mesua ferrea (Nag champa)
+Michelia champaca (Champa)
+Milletia peguensis (Moulmein Rosewood)
+Milletia pinnata (Pongamia)
+Millingtonia hortensis (Indian Cork Tree)
+Mimusops elengi (Moulsari)
+Mitragyna parvifolia (Kaim)
+Monoon longifolium (Mast tree)
+Morinda citrifolia (Noni)
+Morinda pubescens (Indian Mulberry)
+Moringa oleifera (Drumstick tree)
+Morus alba (Silkworm mulberry)
+Mundulea sericea (Cork Bush)
+Muntingia calabura (Singapore/Jamaican cherry)
+Murraya koenigii (Curry-leaf)
+Neolamarckia cadamba (Kadamb)
+Nerium oleander (Oleander)
+Nyctanthes arbor-tristis (Night-flowering jasmine)
+Oncoba spinosa (Snuff-box Tree)
+Oroxylum indicum (Broken Bones Tree)
+Parkia biglandulosa (Badminton-ball tree)
+Parkinsonia (Jerusalem Thorn)
+Peltophorum pterocarpum (Copperpod)
+Phoenix sylvestris (Khajur)
+Phyllanthus emblica (Amla)
+Pimenta dioica (Allspice)
+Pithecellobium dulce (Jungle jalebi)
+Plumeria obtusa (White Farangipani)
+Plumeria rubra (Frangipani)
+Pongamia pinnata (Pongam)
+Premna mollissima (Dusky Firebrand Teak)
+Prosopis cineraria (Khejri)
+Prosopis juliflora (Vilayati keekar)
+Psidium guajava (Guava)
+Pterocarpus marsupium (Indian Kino)
+Pterospermum acerifolium (Kanak champa)
+Pterospermum suberifolium (Kanak Champa)
+Pterygota alata (Buddha's coconut)
+Punica granatum (Pomegranate)
+Putranjiva roxburghii (Putranjiva)
+Reutealis trisperma (Philippine Tung Tree)
+Rhus mysorensis (Indian Sumac)
+Samanea saman (Rain tree)
+Santalum album (Sandalwood)
+Sapindus emarginatus (Notched-leaf Soapnut)
+Saraca asoca (Sita Ashoka)
+Semecarpus anacardium (Oriental cashew)
+Senegalia chundra (Red Cutch Tree)
+Senegalia polyacantha (White Thorn/White Cutch)
+Senna siamea (Siamese Cassia)
+Senna spectabilis (Spectacular Senna)
+Senna surattensis (Scrambled-egg Tree)
+Simaruba glauca (Paradise Tree)
+Soymida febrifuga (Indian Redwood)
+Spathodea campanulata (African Tulip Tree)
+Sterculia foetida (Wild almond)
+Sterculia urens (Gum Karaya/Ghost tree)
+Stereospermum tetragonum (Yellow Snake Tree)
+Streblus asper (Siamese Rough Bush)
+Swietenia macrophylla (Big-leaf Mahogany)
+Swietenia mahogany (Small-leaved Mahogany)
+Syzygium cumini (Jamun)
+Tabebuia aurea (Tree of Gold/Caribbean trumpet tree)
+Tabebuia pallida (Pale Pink Trumpet Tree)
+Tabebuia rosea (Pink poui)
+Tabernaemontana crispa?? (Croaya)
+Tabernaemontana divaricata (Crepe jasmine)
+Tamarindus indica (Tamarind)
+Tecomella undulata (Roheda)
+Tectona grandis (Teak)
+Terminalia arjuna (Arjuna)
+Terminalia bellirica (Beheda)
+Terminalia catappa (Indian Almond)
+Terminalia chebula (Black myrobalan)
+Terminalia tomentosa (Indian laurel)
+Thespesia populnea (Indian tulip tree)
+Vitex negundo (Chaste Tree/Five-leaved Vitex)
+Wodyetia birfurcata (Foxtail Palm)
+Wrightia tinctoria (Dyers oleander)
+Zanthoxylum rhetsa (Indian Prickly Ash)
+Ziziphus mauritiana (Ber)
+Ziziphus xylopyrus (Kath Ber)
 Unidentified / Other`
 
   // const jsonData = Papa.parse(csvData, { header: true }).data;
